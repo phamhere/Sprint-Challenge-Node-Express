510e729ae5fffadeb4cbc02a810679ed9f030947
Peter Pham <phamhere@gmail.com>
2018-11-09 10:01:54 -0700

    untested CRUD for projects and actions

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,8 +1,93 @@
 const express = require("express");
-const actionsDb = require("../data/helpers/actionModel");
+const actionDb = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
-router.get("/");
+router.get("/", (req, res) => {
+  actionDb
+    .get()
+    .then(actions => res.status(200).json(actions))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The actions information could not be retrieved." })
+    );
+});
+
+router.get("/:id", (req, res) => {
+  actionDb
+    .get(req.params.id)
+    .then(action =>
+      action
+        ? res.status(200).json(action)
+        : res.status(404).json({
+            message: "The action with the specified ID does not exist."
+          })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The action information could not be retrieved." })
+    );
+});
+
+router.post("/", (req, res) => {
+  if (req.body.project_id && req.body.description && req.body.notes) {
+    actionDb
+      .insert(req.body)
+      .then(action => res.status(201).json(action))
+      .catch(err =>
+        res.status(500).json({
+          error: "There was an error saving the action to the database."
+        })
+      );
+  } else {
+    res.status(400).json({
+      message:
+        "Please provide a project id, description, and notes for the project."
+    });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  if (req.body.project_id && req.body.description && req.body.notes) {
+    actionDb
+      .update(req.params.id, req.body)
+      .then(action =>
+        action
+          ? res.status(200).json(action)
+          : res.status(404).json({
+              message: "The action with the specified ID does not exist."
+            })
+      )
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "There was an error updating the action." })
+      );
+  } else {
+    res.status(400).json({
+      message:
+        "Please provide a project id, description, and notes for the action."
+    });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  actionDb
+    .remove(req.params.id)
+    .then(count =>
+      count
+        ? res.status(200).json({ message: "Action successfully deleted." })
+        : res.status(404).json({
+            message: "The action with the specified ID does not exist."
+          })
+    )
+    .catch(err =>
+      res.status(500).json({
+        error: "There was an error deleting the action from the database."
+      })
+    );
+});
 
 module.exports = router;
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -3,6 +3,108 @@ const projectDb = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
-router.get("/");
+router.get("/", (req, res) => {
+  projectDb
+    .get()
+    .then(projects => res.status(200).json(projects))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The projects information could not be retrieved." })
+    );
+});
+
+router.get("/:id", (req, res) => {
+  projectDb
+    .get(req.params.id)
+    .then(project =>
+      project
+        ? res.status(200).json(project)
+        : res.status(404).json({
+            message: "The project with the specified ID does not exist."
+          })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The project information could not be retrieved." })
+    );
+});
+
+router.get("/:id/actions", (req, res) => {
+  projectDb
+    .getProjectActions(req.params.id)
+    .then(actions =>
+      actions
+        ? res.status(200).json(actions)
+        : res.status(404).json({
+            message: "The project with the specified ID does not exist."
+          })
+    )
+    .catch(err =>
+      res.status(500).json({
+        error: "The project actions information could not be retrieved."
+      })
+    );
+});
+
+router.post("/", (req, res) => {
+  if (req.body.name && req.body.description) {
+    projectDb
+      .insert(req.body)
+      .then(project => res.status(201).json(project))
+      .catch(err =>
+        res.status(500).json({
+          error: "There was an error saving the project to the database."
+        })
+      );
+  } else {
+    res.status(400).json({
+      message: "Please provide a name and description for the project."
+    });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  if (req.body.name && req.body.description) {
+    projectDb
+      .update(req.params.id, req.body)
+      .then(project =>
+        project
+          ? res.status(200).json(project)
+          : res
+              .status(404)
+              .json({
+                message: "The project with the specified ID does not exist."
+              })
+      )
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "There was an error updating the project." })
+      );
+  } else {
+    res.status(400).json({
+      message: "Please provide a name and description for the project."
+    });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  projectDb
+    .remove(req.params.id)
+    .then(count =>
+      count
+        ? res.status(200).json({ message: "Project successfully deleted." })
+        : res.status(404).json({
+            message: "The project with the specified ID does not exist."
+          })
+    )
+    .catch(err =>
+      res.status(500).json({
+        error: "There was an error deleting the project from the database."
+      })
+    );
+});
 
 module.exports = router;
